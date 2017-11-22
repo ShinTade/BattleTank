2ce6c077e796a81ff906eb073b4d1423ec48ab4e
Tomasz Figas <tomasz.figas@gmail.com>
2017-11-22 12:23:29 +0100

    [Programming] 152 A Better Component Architecture

--- Source/BattleTank/Private/TankMovementComponent.cpp
+++ Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,13 +1,24 @@
 // Copyright C 2017 Tomasz Figas
 
 #include "BattleTank.h"
+#include "TankTrack.h"
 #include "TankMovementComponent.h"
 
-
+void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+}
 
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
 	// auto Name = GetName();
 	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
+
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+
+	//TODO Prevent enabling double-speed bug when pressing two buttons at once
 }
